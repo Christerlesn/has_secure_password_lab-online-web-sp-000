1e7ad0195b3eab2950fe97533c4c70405e92c3d3
Christerle Nounoute <christerlesn@gmail.com>
2021-05-23 23:48:36 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+<<<<<<< HEAD
   def new
     @user = User.new
   end
@@ -18,4 +19,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
   end
+=======
+>>>>>>> f075796c09df5d321fa50e4a8211d1ae28f56c6b
 end
